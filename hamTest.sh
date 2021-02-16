baf2295a3e7fcb423fb1571eca205beedff0336d
Grayson Maguire <graysonmaguire.98@gmail.com>
2021-02-16 15:34:50 +0000

    aadded sh file

---
+++ hamTest.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Request resources:
+#SBATCH -n 1          # 1 CPU core
+#SBATCH --mem=1G      # 1 GB RAM
+#SBATCH --time=6:0:0  # 6 hours (hours:minutes:seconds)
+
+# Run on the queue for serial ("sequential") work
+# (job will share node with other jobs)
+#SBATCH -p seq7.q
+
+# Commands to be run:
+
+module load python/3.6.8
+
+python3 -m pip install numba
+
+python3 hamTest.py
